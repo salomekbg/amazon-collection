700fbe62e7eb419f362b4630c115bbcefcc320d9
SluvsP <salomekbg@gmail.com>
2017-03-05 15:33:08 -0500

    working on syntax for mysql on heroku

--- createdb.php
+++ createdb.php
@@ -1,24 +1,30 @@
 <?php
 
 //connect to MySQL database
-$hostname = "localhost";
-$username = "root";
-$password = "";
-$dbName = "amazon";
+// $hostname = "localhost";
+// $username = "root";
+// $password = "";
+// $dbName = "amazon";
 
-$dbConnected = mysql_connect($hostname, $username, $password);
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$hostname = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$dbName = substr($url["path"], 1);
 
-$dbSuccess = true;
-if (!$dbConnected) {
-  echo "MySQL connection FAILED<br /><br />";
-  $dbSuccess = false;
-}
+$dbConnected = new mysqli($hostname, $username, $password, $dbName);
+
+// $dbSuccess = true;
+// if (!$dbConnected) {
+//   echo "MySQL connection FAILED<br /><br />";
+//   $dbSuccess = false;
+// }
 
 //if connected to database, create database and table if they do not exist
-if ($dbSuccess){
+// if ($dbSuccess){
 
-  $create_db = "Create DATABASE IF NOT EXISTS ".$dbName;
-  if (mysql_query($create_db)) {
+  // $create_db = "Create DATABASE IF NOT EXISTS ".$dbName;
+  // if (mysql_query($create_db)) {
     $create_table = "CREATE TABLE ".$dbName.".collection ( ";
     $create_table .= "ID INT( 10 ) NOT NULL AUTO_INCREMENT PRIMARY KEY, ";
     $create_table .= "ASIN VARCHAR( 50 ) NOT NULL, ";
@@ -27,7 +33,7 @@ if ($dbSuccess){
     $create_table .= "Price VARCHAR( 10 ) NOT NULL";
     $create_table .= ") ";
     mysql_query($create_table);
-  }
-}
+  // }
+// }
 
 ?>
