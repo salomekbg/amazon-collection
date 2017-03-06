40cd4b9e216d87d27d8538c24b355a2b1e50871b
SluvsP <salomekbg@gmail.com>
2017-03-06 15:53:28 -0500

    add more styling for database table

--- showdb.php
+++ showdb.php
@@ -16,20 +16,21 @@ $result = $dbConnected->query($sql);
 //send back a table
 echo "<table style='width: 100%; padding-left: 5px;'>
 <tr>
-  <th style='padding: 5px;'>ASIN</th>
-  <th style='padding: 5px;'>Title</th>
-  <th style='padding: 5px;'>MPN</th>
-</tr>";
+  <th style='padding: 5px; border-bottom: 1px solid black'>ASIN</th>
+  <th style='padding: 5px; border-bottom: 1px solid black'>Title</th>
+  <th style='padding: 5px; border-bottom: 1px solid black'>MPN</th>
+  <th style='padding: 5px; border-bottom: 1px solid black'>Price</th>
+  </tr>";
 
 //put data into the table
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-      echo "<tr>";
-      echo "<td style='padding: 5px;'>" . $row['ASIN'] . "</td>";
-      echo "<td style='padding: 5px;'>" . $row['Title'] . "</td>";
-      echo "<td style='padding: 5px;'>" . $row['MPN'] . "</td>";
-      echo "<td>" . $row['Price'] . "</td>";
+    echo "<tr>";
+      echo "<td style='padding: 5px; border-bottom: 1px solid black'>" . $row['ASIN'] . "</td>";
+      echo "<td style='padding: 5px; border-bottom: 1px solid black'>" . $row['Title'] . "</td>";
+      echo "<td style='padding: 5px; border-bottom: 1px solid black'>" . $row['MPN'] . "</td>";
+      echo "<td style='padding: 5px; border-bottom: 1px solid black'>" . $row['Price'] . "</td>";
       echo "</tr>";
   }
   echo "</table>";
